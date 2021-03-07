d3af5abda1effbe3e9e0c99533d2889919abae43
egboyguda <e.gboyguda@gmail.com>
2021-03-07 16:40:10 +0800

    nag add route para ma view tanan na campgrounds tas nag route para ma view an sa sayu na campground

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path =require('path')
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const { title } = require('process');
+const methodOverride = require('method-override')
 
 //datebase connection
 mongoose.connect('mongodb://localhost/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true})
@@ -20,6 +21,10 @@ const app = express()
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
+//para ma parse an form makuwa an mga value
+app.use(express.urlencoded({extended:true}))
+//para magamit sa form an method na put,delete,patch
+app.use(methodOverride('_method'))
 
 
 // route
@@ -27,12 +32,20 @@ app.get('/',(req,res)=>{
     res.render('home')
 })
 
-//route para maghimu campground
-app.get('/makecampground',async(req,res)=>{
-    const camp = await new Campground({title:'My backyard',description:'barato la na camp'})
-    await camp.save();
-    res.send(camp)
+//route para mag show tanan na campgrounds
+app.get('/campgrounds',async(req,res)=>{
+    const camps = await Campground.find({})
+    res.render('campgrounds/index',{camps})  
 })
+
+//route para ig show an detail sa camp
+//na gin pick
+app.get('/campgrounds/:id',async(req,res)=>{
+    const{id}=req.params
+    const camp = await Campground.findById(id)
+    res.render('campgrounds/camp',{camp})
+})
+
 
 
 
