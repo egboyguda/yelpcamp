d6dc32bd56ac3f09052d60ca84e5e928f6bdd347
egboyguda <e.gboyguda@gmail.com>
2021-03-07 14:23:44 +0800

    adding database

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
 const path =require('path')
+const mongoose = require('mongoose');
 
+//datebase connection
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('DATABASE IS CONNECTED')
+});
 //app.set
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
--- models/campground.js
+++ models/campground.js
@@ -8,3 +8,6 @@ const CampgroundSchema = new Schema({
     description:String,
     location:String
 })
+
+
+module.exports = mongoose.model('Campground',CampgroundSchema);
